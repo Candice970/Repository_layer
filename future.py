85805a189b0e41f8a7458dd321f98fc359dee9ed
Candice970 <pamelacandy57@gmail.com>
2025-04-27 16:53:12 +0200

    Future.py

---
+++ future.py
@@ -0,0 +1,15 @@
+### Future Proofing (future/database_product_repository_stub.py)
+# This is just a stub for later database support
+
+class DatabaseProductRepository(ProductRepository):
+    def save(self, entity):
+        pass
+
+    def find_by_id(self, id):
+        pass
+
+    def find_all(self):
+        pass
+
+    def delete(self, id):
+        pass
